82ee166fef533d96fdef62c95b5066e70042ec80
Gtvsairam <gtvsairam34@gmail.com>
2022-09-05 15:03:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import Form from './Components/Formdet';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Kaisei+Decol&display=swap');
+</style>
+
+ class App extends React.Component {
+      render(){
+        return(
+          <Form/>
+        )
+      }
+    }
+
+
+
+export default App;
